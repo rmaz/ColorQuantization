11db7e720e07aa3f10d2f716460abdfc2483a2e4
Richard Howell <richard@soundcloud.com>
2015-03-09 07:31:36 +0100

    scale the image to 16 bit color before calculating the histogram

--- ImageQuantization/ColorQuantizer.m
+++ ImageQuantization/ColorQuantizer.m
@@ -9,13 +9,13 @@
 #pragma mark - Constants
 
 static const uint16_t kMaxColors = 0x1000;
-static const int kMinPeakDistance = 50;
-static const int kMinEdgeDistance = 50;
+static const int kMinPeakDistance = 200;
+static const int kMinEdgeDistance = 80;
 static const CGSize kImageSize = { 256, 256 };
 
-#define UNPACK_BLUE(index) ((index & 0x0f00) >> 4)
+#define UNPACK_RED(index) ((index & 0x0f00) >> 4)
 #define UNPACK_GREEN(index) (index & 0x00f0)
-#define UNPACK_RED(index)  ((index & 0x000f) << 4)
+#define UNPACK_BLUE(index)  ((index & 0x000f) << 4)
 
 #pragma mark - Init
 
@@ -43,7 +43,7 @@ static const CGSize kImageSize = { 256, 256 };
     NSLog(@"resampling time %.0f ms", 1000*(t2 - t1));
 
     t1 = CFAbsoluteTimeGetCurrent();
-    [self calculateHistogramWithImageData:imageData length:kImageSize.width * kImageSize.height];
+    [self calculateHistogramWithImageData:imageData];
     t2 = CFAbsoluteTimeGetCurrent();
     NSLog(@"histogram calculation took %.3f ms\n", 1000*(t2-t1));
 
@@ -59,12 +59,12 @@ static const CGSize kImageSize = { 256, 256 };
 
 - (NSData *)scaledImageDataFromImage:(UIImage *)image size:(CGSize)size
 {
-    NSUInteger length = 4 * size.width * size.height;
+    NSUInteger length = sizeof(uint16_t) * size.width * size.height;
     void *data = malloc(length);
 
     CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
-    CGContextRef ctx = CGBitmapContextCreate(data, size.width, size.height, 8, size.width * 4, colorSpace,
-                                             kCGBitmapByteOrderDefault | kCGImageAlphaNoneSkipLast);
+    CGContextRef ctx = CGBitmapContextCreate(data, size.width, size.height, 5, size.width * sizeof(uint16_t), colorSpace,
+                                             kCGBitmapByteOrder16Little | kCGImageAlphaNoneSkipFirst);
     CGContextDrawImage(ctx, CGRectMake(0, 0, size.width, size.height), image.CGImage);
     CGContextRelease(ctx);
     CGColorSpaceRelease(colorSpace);
@@ -72,15 +72,18 @@ static const CGSize kImageSize = { 256, 256 };
     return [[NSData alloc] initWithBytesNoCopy:data length:length];
 }
 
-- (void)calculateHistogramWithImageData:(NSData *)imageData length:(size_t)length
+- (void)calculateHistogramWithImageData:(NSData *)imageData
 {
     bzero(_histogram, kMaxColors * sizeof(_histogram[0]));
 
-    uint32_t *pixel = (uint32_t *)imageData.bytes;
-    for (size_t i = 0; i < length; i++) {
-        uint32_t value = *pixel;
-        uint16_t index = ((value & 0xf00000) >> 12) + ((value & 0xf000) >> 8) + ((value & 0xf0) >> 4);
-        _histogram[index]++;
+    NSUInteger *pixel = (NSUInteger *)imageData.bytes;
+    for (size_t i = 0; i < [imageData length]; i += sizeof(NSUInteger)) {
+
+        for (int p = 0; p < sizeof(NSUInteger) / sizeof(uint16_t); p++) {
+            NSUInteger value = *pixel >> (p * 16);
+            uint16_t index = ((value & 0x7800) >> 3) | ((value & 0x3c0) >> 2) | ((value & 0x1e) >> 1);
+            _histogram[index]++;
+        }
         pixel++;
     }
 }
